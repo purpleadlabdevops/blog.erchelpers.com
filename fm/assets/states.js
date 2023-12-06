d4f57a02978f6f2cdc4fe0f302e7a79dcd8bd1e8
Max Globa <onyx18121990@gmail.com>
2023-12-06 20:59:28 +0300

    1

---
+++ fm/assets/states.js
@@ -0,0 +1,19 @@
+const successCallback = (position) => {
+  console.log(position)
+  console.log(position.coords.latitude)
+  console.log(position.coords.longitude)
+  const latitude = 35.67524927287536
+  const longitude = -118.78036309281335
+  $.ajax({ url:'//maps.googleapis.com/maps/api/geocode/json?latlng='+latitude+','+longitude+'&sensor=true',
+    success: function(data){
+      var state = data.results[0].address_components[5].long_name;
+      console.log(state);
+    }
+  })
+}
+
+const errorCallback = (error) => {
+  console.log(error)
+}
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
