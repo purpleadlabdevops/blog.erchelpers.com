346f7e9d3bdc41eb5ee473853c53291f0bda607f
Max Globa <onyx18121990@gmail.com>
2023-12-07 13:28:44 +0300

    Update states.js

--- fm/assets/states.js
+++ fm/assets/states.js
@@ -1,15 +1,10 @@
-const successCallback = (position) => {
-  console.log(position)
-  console.log(position.coords.latitude)
-  console.log(position.coords.longitude)
-  const latitude = 35.67524927287536
-  const longitude = -118.78036309281335
-  $.ajax({ url:'//maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDWS8pvsYm6Ap0nMJdatFU9l52xpW-zqHw&latlng='+latitude+','+longitude+'&sensor=true',
-    success: function(data){
-      console.dir(data)
-      var state = data.results[0].address_components[3].long_name;
-      console.log('State - '+state);
-      document.getElementById('setState').innerHTML = state
+const successCallback = position => {
+  $.ajax({ url:'//maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDWS8pvsYm6Ap0nMJdatFU9l52xpW-zqHw&latlng='+position.coords.latitude+','+position.coords.longitude+'&sensor=true',
+    success: data => {
+      const state = data.results[0].address_components[3].long_name;
+      if(state){
+        document.getElementById('setState').innerHTML = `in ${state}`
+      }
     }
   })
 }
