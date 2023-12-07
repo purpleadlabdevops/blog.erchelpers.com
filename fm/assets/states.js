c05d743887e229a8894b279c11873e619df83a24
Max Globa <onyx18121990@gmail.com>
2023-12-07 20:01:43 +0300

    Update states.js

--- fm/assets/states.js
+++ fm/assets/states.js
@@ -2,6 +2,7 @@ const successCallback = position => {
   $.ajax({ url:'//maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDWS8pvsYm6Ap0nMJdatFU9l52xpW-zqHw&latlng='+position.coords.latitude+','+position.coords.longitude+'&sensor=true',
     success: data => {
       const state = data.results[0].address_components[3].long_name;
+      console.dir(data)
       if(state){
         document.getElementById('setState').innerHTML = `in ${state}`
       }
